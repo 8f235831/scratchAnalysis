a346490ebfed3e214db20bae3e92e5a9acf47143
spring is gone <55890552+dhkdhk@users.noreply.github.com>
2021-12-31 14:39:28 +0800

    Add files via upload

--- 20211230-scratchAnalysis/src/main/resources/static/js/index.js
+++ 20211230-scratchAnalysis/src/main/resources/static/js/index.js
@@ -1,14 +1,40 @@
 $(document).ready(function () {
     // with plugin options
     $("#input-id").fileinput({
-        uploadUrl: "/test",
-        uploadAsync: false,
+        uploadUrl: "/sourceSubmit",
+        // uploadAsync: false,
         language: 'zh',
-    }).on('fileuploaded', function (event, previewId, index, fileId) {
-        console.log('File Uploaded', 'ID: ' + fileId + ', Thumb ID: ' + previewId);
-    }).on('fileuploaderror', function (event, data, msg) {
-        console.log('File Upload Error', 'ID: ' + data.fileId + ', Thumb ID: ' + data.previewId);
-    }).on('filebatchuploadcomplete', function (event, preview, config, tags, extraData) {
-        console.log('File Batch Uploaded', preview, config, tags, extraData);
-    });
+    })
+    $("#input-id").on("fileuploaded", function (event, data, previewId, index) {
+     console.log('event = ', event)
+        console.log('data = ', data)
+        let rows = data.response.results;
+        $(".remove").remove();
+        for(var i = 0; i < rows.length; i++) {
+            var name = rows[i].name;
+            var max_score = rows[i].max_score;
+            var score = rows[i].score;
+            var html = $(".tr1clone").clone();
+            html.removeClass("tr1clone").show().addClass("remove");
+            html.find(".td1").text(name)
+            html.find(".score").text(score)
+            html.find(".max_score").text(max_score)
+            if(score == 0){
+                html.find(".progress-bar0").show();
+            }
+            if(score == 1){
+                html.find(".progress-bar1").show();
+            }
+            if(score == 2){
+                html.find(".progress-bar-warning").show();
+            }
+            if(score == 3){
+                html.find(".progress-bar-success").show();
+            }
+
+            $("#tb1").append(html);
+        }
+
+
+    })
 });
